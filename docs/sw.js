2eed175607e54a859fd153fd609decbbfab7b540
monitoramentov3lan <monitoramentov3lan@gmail.com>
2026-08-06 15:28:22 -0300

    UPDATE APP

--- docs/sw.js
+++ docs/sw.js
@@ -1,67 +1,39 @@
-const VERSION = "1.0.0";
-
+const VERSION = "1.0.1";
 const CACHE = "v3lan-" + VERSION;
 
 const FILES = [
-
     "./",
-
     "./index.html",
-
+    "./garagens.js",
     "./manifest.json"
-
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE)
-
         .then(cache => cache.addAll(FILES))
-
     );
-
     self.skipWaiting();
-
 });
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
         caches.keys()
-
         .then(keys =>
-
             Promise.all(
-
                 keys
-
                 .filter(key => key !== CACHE)
-
                 .map(key => caches.delete(key))
-
             )
-
         )
-
     );
-
     self.clients.claim();
-
 });
 
 self.addEventListener("fetch", event => {
-
     if (event.request.method !== "GET") return;
-
     event.respondWith(
-
         fetch(event.request)
-
         .catch(() => caches.match(event.request))
-
     );
-
 });
